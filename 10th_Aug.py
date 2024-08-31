b31d038917d446feb2e6a5ce6c18dbf33a911262
chandanpramod <pamu.chandan007@gmail.com>
2024-08-31 10:23:30 +0530

    My commit

--- 10th_Aug.py
+++ 10th_Aug.py
@@ -5,3 +5,9 @@ print("Pramod","Bansi","Chandan",sep="-",end='  ')
 print("Pramod","Bansi","Chandan",sep="-",end='  ')
 print("Pramod","Bansi","Chandan",sep="-",end='_')
 print("Pramod","Bansi","Chandan",sep="-",end='_')
+
+# print(max(34,100))
+
+print(max(1,2,5,100,"Pramod"))
+# TypeError: '>' not supported between instances of 'str' and 'int'
+# -- error
---
+++ debugger_10thAug.py
@@ -0,0 +1,5 @@
+a=20
+b=10
+c=30
+sum=(a+b+c)
+print(sum)
