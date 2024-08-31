2169a6188baa946db575608ae52916e23ca638ed
chadanpramod <pramodbchandan@gmail.com>
2024-08-31 08:53:08 +0530

    My commit

---
+++ 10th_Aug.py
@@ -0,0 +1,7 @@
+print("Pramod","Bansi","Chandan",sep="-",end='  ')
+print("Pramod","Bansi","Chandan",sep="-",end='  ')
+print("Pramod","Bansi","Chandan",sep="-",end='  ')
+print("Pramod","Bansi","Chandan",sep="-",end='  ')
+print("Pramod","Bansi","Chandan",sep="-",end='  ')
+print("Pramod","Bansi","Chandan",sep="-",end='_')
+print("Pramod","Bansi","Chandan",sep="-",end='_')
